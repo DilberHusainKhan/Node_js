eb658844f3b6b7cdd856280182b20344676314a5
dilberhusainkhan <dilberjmi@gmail.com>
2023-09-26 21:18:18 +0530

    Update

--- basicJS/Ques_reduce.js
+++ basicJS/Ques_reduce.js
@@ -1,6 +1,8 @@
 const person=[
     {firstName: "Dilber", lastName : "Khan",age : 24},
     {firstName: "Humayun", lastName : "Aanwar",age : 25},
+    {firstName: "KKK", lastName : "Aanwar",age : 35},
+    {firstName: "AAAA", lastName : "Aanwar",age : 45},
     {firstName: "Aadil", lastName : "Rana",age : 24},
 ]
 
@@ -15,3 +17,8 @@ const output = person.reduce((acc,curr)=>{
     return acc;
 },{}) 
 console.log(output);
+// Print the first name of persion whose age is less than 30
+const output1 = person.filter(x=> x.age<30).map(x=>x.firstName)
+
+
+console.log(output1);
